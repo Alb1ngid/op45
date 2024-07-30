4eecfb6dcead881856cccb1b1bd0621952a7b879
Geek_Tech <geecktech.och2@gmail.com>
2024-07-30 20:36:38 +0600

    'lesson1'

---
+++ lessons/lesson1.py
@@ -0,0 +1,12 @@
+# VENV, CMD,GIT
+# модули в языке
+# 3 вида встроенные , собвствненные и внешние
+
+p = 1,'str',True,1.2,[],{},(),None
+
+
+if __name__ == '__main__':
+    print(type(p))
+
+import random
+
---
+++ lessons/test.py
@@ -0,0 +1,14 @@
+import lesson1
+import colorama
+from art import tprint
+
+print(colorama.Back.BLACK
+      ,colorama.Fore.GREEN)
+print('Hello World')
+print(lesson1.p)
+
+
+
+tprint('Hello World')
+
+
